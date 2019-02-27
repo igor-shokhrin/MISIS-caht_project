5cfc36c4a26017ebd59ee348389188a4e06038a4
NanFe <lestrange107@gmail.com>
2019-02-27 18:35:57 +0300

    First

---
+++ front/index.php
@@ -0,0 +1,32 @@
+<html class="no-js" lang="ru"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="Cache-Control" content="no-store">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title id="page_title">ЧАТИК</title>
+
+	
+
+<style type="text/css"></style></head>
+
+    <body class="start-page" data-lang-photoregisteredonly="" data-lang-buttonregister="">
+        <div id="wrapper">       
+            <div class="chatContent">			
+                <div id="page">
+                    <div id="header" class="clearfix">
+                        <h1>ЧатО</h1>
+                        <h2>Пора общаться</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<div id="side" class="push-available">
+	<div id="side-content">	
+		<div id="contacts-available" class="hidden">
+			<div class="menu-header">
+				<p>Контакты:<a href="https://github.com/igor-shokhrin/MISIS-chat_project">Git</a>
+				</p>
+			</div>
+		</div>		
+</body></html>
