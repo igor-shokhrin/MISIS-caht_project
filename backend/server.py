def96ab9fb610a7e2712f9f8a204fec67ebaaffc
Igor Shokhrin <i.shokhrin@iva-tech.ru>
2019-03-06 16:23:50 +0300

    Support MS db was added

---
+++ backend/server.py
@@ -0,0 +1,27 @@
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import urllib
+import sqlalchemy
+
+
+if __name__ == "__main__":
+
+    params = urllib.parse.quote_plus('Driver={SQL Server};'
+                                     'Server=DESKTOP-SN1834C\SQLEXPRESS;'
+                                     'Database=Server;')
+    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
+    """
+    table = Table('login', meta,
+   	Column('id', Integer, primary_key=True),
+   	Column('usr', String),
+   	Column('password', String))
+    table = Table('Mess', meta,
+   	Column('id', Integer, primary_key=True),
+   	Column('usr', String),
+   	Column('mess', String))
+    meta.create_all()
+    """
+    conn = engine.connect()
+    meta = sqlalchemy.MetaData(engine, reflect=True)
+    table_l = meta.tables['login']
+    table_m = meta.tables['Mess']
