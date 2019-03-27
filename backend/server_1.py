b4b49b9d3161cc61bd0f07f337b9232b156d92e6
Igor Shokhrin <i.shokhrin@iva-tech.ru>
2019-03-27 16:53:26 +0300

    Structure changed

--- backend/server_1.py
+++ backend/server_1.py
@@ -4,8 +4,20 @@ app = Flask(__name__)
 
 @app.route('/for_test', methods=['POST'])
 def for_test():
-    test_value = request.form["test_name"]
-    return render_template('for_test.html', test_value=test_value)
+    input_json = request.form["test_name"]
+    print('data from client:', input_json)
+    print(input_json['cmd'])
+    if (input_json['cmd'] == 'VK_Autorization'):
+        try:
+            kontakt.get_user_photo(input_json['username'], input_json['password'])
+        except vk.exceptions.VkAuthError:
+            return render_template('for_test.html', {'answer': 'Incorrect username or password'})
+    return render_template('for_test.html', {'answer': 'Autorization OK'})
+    #return render_template('for_test.html', test_value=test_value)
+
+@app.route('/')
+def home():
+        return render_template('for_test.html')
 
 @app.route('/tests/endpoint', methods=['POST'])
 def my_test_endpoint():
