f70c3b4308a58420aab76a49e9cab7d6c339d62b
igor-shokhrin <igor.shokhrin@gmail.com>
2019-04-02 21:53:18 +0300

    Structure changes

--- backend/Client_1.py
+++ backend/Client_1.py
@@ -1,5 +1,6 @@
 import requests
-dictToSend = {'cmd':'VK_Autorization', 'username' : "+79773947459", 'password':"123as"}
+# dictToSend = {'cmd':'VK_Autorization', 'username' : "+79773947459", 'password':"123as"}
+dictToSend = {'cmd':'get_msg'}
 res = requests.post('http://localhost:5000/tests/endpoint', json=dictToSend)
 print('response from server:',res.text)
 dictFromServer = res.json()
